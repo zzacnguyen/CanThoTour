eb5e24bf59d0811641610f0df40d50f9d15961e4
huy7644 <32673667+huy7644@users.noreply.github.com>
2017-12-08 00:26:30 +0700

    sửa load danh sách vui chơi

--- app/src/main/java/com/doan3/canthotour/View/Main/Content/ActivityEntertainment.java
+++ app/src/main/java/com/doan3/canthotour/View/Main/Content/ActivityEntertainment.java
@@ -26,6 +26,7 @@ import com.doan3.canthotour.View.Personal.ActivityPersonal;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -86,7 +87,8 @@ public class ActivityEntertainment extends AppCompatActivity {
             super.onPostExecute(s);
             try {
                 // parse json ra arraylist
-                ArrayList<String> arrayList = JsonHelper.parseJson(new JSONArray(s), Config.JSON_ENTERTAINMENT);
+                ArrayList<String> arr = JsonHelper.parseJsonNoId(new JSONObject(s), Config.JSON_LOAD);
+                ArrayList<String> arrayList = JsonHelper.parseJson(new JSONArray(arr.get(0)), Config.JSON_ENTERTAINMENT);
 
                 RecyclerView recyclerView = findViewById(R.id.RecyclerView_DanhSachVuiChoi);
                 recyclerView.setHasFixedSize(true); //Tối ưu hóa dữ liệu, k bị ảnh hưởng bởi nội dung trong adapter
