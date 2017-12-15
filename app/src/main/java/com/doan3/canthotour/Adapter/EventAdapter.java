4abcc7554bcbbe5d13ee3eb1009bb61da18ff963
huy7644 <32673667+huy7644@users.noreply.github.com>
2017-12-15 07:27:56 +0700

    thêm eventadapter

---
+++ app/src/main/java/com/doan3/canthotour/Adapter/EventAdapter.java
@@ -0,0 +1,135 @@
+package com.doan3.canthotour.Adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.doan3.canthotour.Interface.OnLoadMoreListener;
+import com.doan3.canthotour.Model.Eat;
+import com.doan3.canthotour.Model.Event;
+import com.doan3.canthotour.R;
+import com.doan3.canthotour.View.Main.ActivityEatInfo;
+import com.doan3.canthotour.View.Main.ActivityPlaceInfo;
+
+import java.util.ArrayList;
+
+
+public class EventAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private final int VIEW_TYPE_ITEM = 0;
+    private final int VIEW_TYPE_LOADING = 1;
+    private OnLoadMoreListener onLoadMoreListener;
+    private boolean isLoading;
+    private Context context;
+    private ArrayList<Event> events;
+    private int visibleThreshold = 5;
+    private int lastVisibleItem, totalItemCount;
+
+    public EventAdapter(RecyclerView recyclerView, ArrayList<Event> events, Context context) {
+        this.context = context;
+        this.events = events;
+
+        final LinearLayoutManager linearLayoutManager = (LinearLayoutManager) recyclerView.getLayoutManager();
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                totalItemCount = linearLayoutManager.getItemCount();
+                lastVisibleItem = linearLayoutManager.findLastVisibleItemPosition();
+                if (!isLoading && totalItemCount <= (lastVisibleItem + visibleThreshold)) {
+                    if (onLoadMoreListener != null)
+                        onLoadMoreListener.onLoadMore();
+                    isLoading = true;
+                }
+            }
+        });
+    }
+
+    public void setOnLoadMoreListener(OnLoadMoreListener mOnLoadMoreListener) {
+        this.onLoadMoreListener = mOnLoadMoreListener;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return events.get(position) == null ? VIEW_TYPE_LOADING : VIEW_TYPE_ITEM;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        if (viewType == VIEW_TYPE_ITEM) {
+            View view = LayoutInflater.from(context).inflate(R.layout.custom_thongbao, parent, false);
+            return new ViewHolder(view);
+        } else if (viewType == VIEW_TYPE_LOADING) {
+            View view = LayoutInflater.from(context).inflate(R.layout.item_loading, parent, false);
+            return new LoadingViewHolder(view);
+        }
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, final int position) {
+        if (holder instanceof ViewHolder) {
+            Event event = events.get(position);
+            ViewHolder viewHolder = (ViewHolder) holder;
+            viewHolder.txtTenSk.setText(event.getTenSk());
+            viewHolder.txtNgaySk.setText(event.getNgaySk());
+            viewHolder.imgHinhSk.setImageResource(event.getHinhSk());
+
+//            viewHolder.cardView.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View view) {
+//                    Intent iEatInfo = new Intent(context, ActivityPlaceInfo.class);
+//                    iEatInfo.putExtra("masp", position + "");
+//                    iEatInfo.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//                    context.startActivity(iEatInfo);
+//                }
+//            });
+        } else if (holder instanceof LoadingViewHolder) {
+            LoadingViewHolder loadingViewHolder = (LoadingViewHolder) holder;
+            loadingViewHolder.progressBar.setIndeterminate(true);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return events.size();
+    }
+
+    public void setLoaded() {
+        isLoading = false;
+    }
+
+    // "Loading item" ViewHolder
+    private class LoadingViewHolder extends RecyclerView.ViewHolder {
+        public ProgressBar progressBar;
+
+        public LoadingViewHolder(View view) {
+            super(view);
+            progressBar = (ProgressBar) view.findViewById(R.id.progressBar);
+        }
+    }
+
+    //"Normal item" Viewholder
+    private class ViewHolder extends RecyclerView.ViewHolder { //ViewHolder chạy thứ 2, phần này giúp cho recycler view ko bị load lại dữ liệu khi thực hiện thao tác vuốt màn hình
+        TextView txtTenSk, txtNgaySk;
+        ImageView imgHinhSk;
+        CardView cardView;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            txtTenSk = itemView.findViewById(R.id.textViewTenSk);
+            imgHinhSk = itemView.findViewById(R.id.imageViewSuKien);
+            txtNgaySk = itemView.findViewById(R.id.textViewNgaySk);
+            cardView = (CardView) itemView.findViewById(R.id.cardViewSuKien);
+
+        }
+    }
+}
