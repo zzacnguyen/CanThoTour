2ef9ce2d91f413e9c503c2d4f7d34ef0624c8002
zzacnguyen <zzacnguyen@gmail.com>
2018-01-04 01:05:08 +0700

    Tạo thêm menu khoảng cách lân cận bên layout cài đặt
    Chỉnh sửa khỏang cách lân cận dùng palette đc thêm từ plugin bên ngoài là BubbleSeekBar, về cơ bản cũng có các function giống như SeekBar, có thể get đc value,....
    T làm kiểu seekbar này xem ổn ko, nếu thấy ko êm thì t sửa tiếp.
    Về hình ảnh t chưa load đc hình từ máy lên nên chưa gắn vào, mai t làm tiếp.

--- app/src/main/java/com/doan3/canthotour/View/Personal/ActivityRangeSetting.java
+++ app/src/main/java/com/doan3/canthotour/View/Personal/ActivityRangeSetting.java
@@ -8,32 +8,45 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
+import android.widget.TextSwitcher;
+import android.widget.TextView;
 
 import com.doan3.canthotour.Helper.BottomNavigationViewHelper;
 import com.doan3.canthotour.R;
 import com.doan3.canthotour.View.Favorite.ActivityFavorite;
 import com.doan3.canthotour.View.Main.MainActivity;
 import com.doan3.canthotour.View.Notify.ActivityNotify;
+import com.xw.repo.BubbleSeekBar;
 
 
-public class ActivityOption extends AppCompatActivity {
-    Button btnKhoangCachLc;
+public class ActivityRangeSetting extends AppCompatActivity {
+    BubbleSeekBar bubbleSeekBar;
+    TextView txtSeekView;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_caidat);
+        setContentView(R.layout.activity_caidatkhoangcach);
 
-        btnKhoangCachLc = findViewById(R.id.buttonKhoanCachLc);
+        bubbleSeekBar = findViewById(R.id.sbKhoangCach);
+        txtSeekView = findViewById(R.id.textViewSeekView);
 
-        btnKhoangCachLc.setOnClickListener(new View.OnClickListener() {
+        bubbleSeekBar.setOnProgressChangedListener(new BubbleSeekBar.OnProgressChangedListener() {
             @Override
-            public void onClick(View view) {
-                Intent iKhoangCach = new Intent(ActivityOption.this, ActivityRangeSetting.class);
-                startActivity(iKhoangCach);
+            public void onProgressChanged(BubbleSeekBar bubbleSeekBar, int progress, float progressFloat) {
+                txtSeekView.setText(String.format("%d",progress));
+            }
+
+            @Override
+            public void getProgressOnActionUp(BubbleSeekBar bubbleSeekBar, int progress, float progressFloat) {
+
+            }
+
+            @Override
+            public void getProgressOnFinally(BubbleSeekBar bubbleSeekBar, int progress, float progressFloat) {
+
             }
         });
+
 
         menuBotNarBar();
     }
@@ -51,16 +64,16 @@ public class ActivityOption extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()){
                     case R.id.ic_trangchu:
-                        startActivity(new Intent(ActivityOption.this, MainActivity.class));
+                        startActivity(new Intent(ActivityRangeSetting.this, MainActivity.class));
                         break;
                     case R.id.ic_yeuthich:
-                        startActivity(new Intent(ActivityOption.this, ActivityFavorite.class));
+                        startActivity(new Intent(ActivityRangeSetting.this, ActivityFavorite.class));
                         break;
                     case R.id.ic_thongbao:
-                        startActivity(new Intent(ActivityOption.this, ActivityNotify.class));
+                        startActivity(new Intent(ActivityRangeSetting.this, ActivityNotify.class));
                         break;
                     case R.id.ic_canhan:
-                        startActivity(new Intent(ActivityOption.this, ActivityPersonal.class));
+                        startActivity(new Intent(ActivityRangeSetting.this, ActivityPersonal.class));
                         break;
                 }
                 return false;
