e096d58692515afd8747227a70c1ff2cebd88a9c
zzacnguyen <zzacnguyen@gmail.com>
2018-01-31 15:42:46 +0700

    Chọn địa điểm trên bản đồ
    Dùng Google Place Picker chọn địa điểm trên bản đồ sau đó lấy về kinh độ, vĩ độ, thông tin nếu có sẵn. Tìm các địa điểm gần đó

--- app/src/main/java/com/doan3/canthotour/View/Personal/ActivityAddPlace.java
+++ app/src/main/java/com/doan3/canthotour/View/Personal/ActivityAddPlace.java
@@ -8,24 +8,86 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.doan3.canthotour.Helper.BottomNavigationViewHelper;
 import com.doan3.canthotour.R;
 import com.doan3.canthotour.View.Favorite.ActivityFavorite;
 import com.doan3.canthotour.View.Main.MainActivity;
 import com.doan3.canthotour.View.Notify.ActivityNotify;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
 
 /**
  * Created by zzacn on 12/7/2017.
  */
 
 public class ActivityAddPlace extends AppCompatActivity {
+
+    private final int REQUEST_CODE_PLACEPICKER = 1;
+    TextView txtLat, txtLong;
+    EditText etAddress, etPlaceName;
+    Button btnPlacePicker;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addlocation);
 
+        txtLat = findViewById(R.id.txtLatitude);
+        txtLong = findViewById(R.id.txtLongitude);
+        etAddress = findViewById(R.id.etAddress);
+        etPlaceName = findViewById(R.id.etPlaceName);
+        btnPlacePicker = findViewById(R.id.btnPlacePicker);
+
+        btnPlacePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startPlacePickerActivity();
+            }
+        });
+
         menuBotNarBar();
+    }
+
+    private void startPlacePickerActivity() {
+        PlacePicker.IntentBuilder intentBuilder = new PlacePicker.IntentBuilder();
+
+        try {
+            Intent intent = intentBuilder.build(this);
+            startActivityForResult(intent, REQUEST_CODE_PLACEPICKER);
+        } catch (GooglePlayServicesRepairableException e) {
+            e.printStackTrace();
+        } catch (GooglePlayServicesNotAvailableException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == REQUEST_CODE_PLACEPICKER && resultCode == RESULT_OK){
+            displaySelectedPlaceFromPlacePicker(data);
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void displaySelectedPlaceFromPlacePicker(Intent data) {
+        Place placeSelected = PlacePicker.getPlace(data,this);
+
+        Double latitude = placeSelected.getLatLng().latitude;
+        Double longitude = placeSelected.getLatLng().longitude;
+
+        txtLat.setText(String.valueOf(latitude).substring(0,8));
+        txtLong.setText(String.valueOf(longitude).substring(0,10));
+        etAddress.setText(placeSelected.getAddress().toString());
+        etPlaceName.setText(placeSelected.getName().toString());
     }
 
     private void menuBotNarBar() {
