6cf4eae096f28734ef6acbd5fce905c84ba9b3e3
zzacnguyen <zzacnguyen@gmail.com>
2018-05-08 23:32:51 +0700

    Cập nhật công việc tuần
    - Sửa dấu gạch ngang trong danh sách đánh giá
    - Tạo thêm button general để chứa thông tin ứng dụng
    - Tạo thêm button set khoàng cách lân cận trong danh sách lân cận. Đã copy hàm để mở dialog và các khai báo, chưa code hoàn chỉnh

--- app/src/main/java/com/doan3/canthotour/View/Search/ActivityNearLocation.java
+++ app/src/main/java/com/doan3/canthotour/View/Search/ActivityNearLocation.java
@@ -1,6 +1,8 @@
 package com.doan3.canthotour.View.Search;
 
 
+import android.annotation.SuppressLint;
+import android.app.Dialog;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -8,6 +10,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -16,6 +21,7 @@ import com.doan3.canthotour.Config;
 import com.doan3.canthotour.Model.ModelSearch;
 import com.doan3.canthotour.Model.ObjectClass.NearLocation;
 import com.doan3.canthotour.R;
+import com.doan3.canthotour.View.Personal.ActivityOption;
 
 import java.util.ArrayList;
 
@@ -24,6 +30,9 @@ import static com.doan3.canthotour.View.Main.MainActivity.menuBotNavBar;
 public class ActivityNearLocation extends AppCompatActivity {
     TextView txtPlaceName, txtRadius;
     ImageView imgPlacePhoto;
+    ImageView btnSetRadius, btnBack;
+    String radius;
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -32,11 +41,55 @@ public class ActivityNearLocation extends AppCompatActivity {
         txtPlaceName = findViewById(R.id.textViewNearName);
         txtRadius = findViewById(R.id.textViewRadius);
         imgPlacePhoto = findViewById(R.id.imageViewNear);
+        btnSetRadius = findViewById(R.id.btnSetRadius);
+        btnBack = findViewById(R.id.btnBack);
 
         String latitude = getIntent().getStringExtra(Config.KEY_NEAR_LOCATION.get(0));
         String longitude = getIntent().getStringExtra(Config.KEY_NEAR_LOCATION.get(1));
         int serviceType = getIntent().getIntExtra(Config.KEY_NEAR_LOCATION.get(2), 1);
         searchNearLocation(latitude, longitude, serviceType);
+
+        btnSetRadius.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Dialog dialog = new Dialog(ActivityNearLocation.this);
+                dialog.setTitle(getResources().getString(R.string.text_SetDistance));
+                dialog.setCancelable(false); //Khóa màn hình ngoài sau khi ấn vàodialog
+                dialog.setContentView(R.layout.custom_radius);
+
+                //Ánh xạ các palette trong dialog
+                final EditText etDistance = dialog.findViewById(R.id.etRadius);
+                Button btnAgree = dialog.findViewById(R.id.btnConfirmRadius);
+                Button btnCancel = dialog.findViewById(R.id.btnCancelRadius);
+
+//                btnAgree.setOnClickListener(new View.OnClickListener() {
+//                    @SuppressLint("SetTextI18n")
+//                    @Override
+//                    public void onClick(View view) {
+//                        radius = etDistance.getText().toString().trim();
+//                        editor.putString(Config.KEY_DISTANCE, radius);
+//                        editor.apply();
+//                        dialog.cancel();
+//                    }
+//                });
+
+                btnCancel.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        dialog.cancel();
+                    }
+                });
+
+                dialog.show();
+            }
+        });
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
 
         menuBotNavBar(this, 0);
     }
